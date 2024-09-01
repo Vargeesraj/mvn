f21ff977582199c832b45b7f676dc501a99c2e87
Raj <Raj@DESKTOP-VGL160M>
2024-09-01 13:00:12 +0530

    first time

---
+++ mvn/src/test/java/com/actitime/testscipt/CustomerModule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscipt;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+@Test
+public void creatCustomer()
+{
+	Reporter.log("createCustomer",true);
+}
+}
